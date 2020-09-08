11ad7028524c9e092a31d781ba7eada9ba14e2b0
ace86lcn <ace86lcn@163.com>
2020-09-08 15:25:01 +0800

    tab栏

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <m-header></m-header>
-    <!-- <router-view/> -->
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import MHeader from './views/m-header/m-header'
+import Tab from './views/tab/tab'
 export default {
   name: 'App',
   components: {
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Recommend from '../views/recommend/recommend'
+import Singer from '../views/singer/singer'
+import Rank from '../views/rank/rank'
+import Search from '../views/search/search'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/header',
-      components: '@/views/header/header'
+      path: '/',
+      redirect: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      component: Search
     }
   ]
 })
