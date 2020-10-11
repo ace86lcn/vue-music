f4d47476539a2ec594aef0571bd993a487b31414
lcn <ace86lcn@163.com>
2020-10-11 10:19:20 +0800

    推荐列表热门歌单页面

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
 fastclick.attach(document.body)
 
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="recommend" ref="recommend">
-        <scroll ref="scroll" class="recommend-content">
+        <scroll ref="scroll" class="recommend-content" :data="discList">
             <div>
                 <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
                     <slider>
                         <div v-for="(item, index) in recommends" :key="index">
+                          <!-- needsclick 解决fastclick点击冲突的问题 -->
                         <a :href="item.linkUrl">
-                            <img class="needsclick"  :src="item.picUrl">
+                            <img class="needsclick"  :src="item.picUrl" @load="loadImage">
                         </a>
                         </div>
                     </slider>
@@ -14,9 +15,20 @@
                 <div class="recommend-list">
                   <h1 class="list-title">热门歌单推荐</h1>
                   <ul>
-                    <li v-for="(item, index) in discList" :key="index"></li>
+                    <li v-for="(item, index) in discList" :key="index" class="item">
+                      <div class="icon">
+                        <img width="60" height="60" v-lazy="item.imgurl">
+                      </div>
+                      <div class="text">
+                        <h2 class="name" v-html="item.creator.name"></h2>
+                        <p class="desc" v-html="item.dissname"></p>
+                      </div>
+                    </li>
                   </ul>
                 </div>
+            </div>
+            <div class="loading-container" v-show="!discList.length">
+              <loading></loading>
             </div>
         </scroll>
     </div>
@@ -24,12 +36,14 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
+import Loading from 'base/loading/loading'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 export default {
   components: {
     Scroll,
-    Slider
+    Slider,
+    Loading
   },
   data () {
     return {
@@ -49,10 +63,17 @@ export default {
     // 歌单
     _getDiscList () {
       getDiscList().then((res) => {
-        if (res.cude === ERR_OK) {
+        if (res.code === ERR_OK) {
           this.discList = res.data.list
         }
       })
+    },
+    // 轮埠图片高度撑开
+    loadImage () {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        this.$refs.Scroll.refresh()
+      }
     }
   },
   created () {
