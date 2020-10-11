d6f5d69f07e810c3768a57b3b7b901af653515e0
lcn <ace86lcn@163.com>
2020-10-11 10:43:33 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import fastclick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
-import VueLazyload from 'vue-lazyload'
 import router from './router'
 import 'common/stylus/index.styl'
 import VueLazyload from 'vue-lazyload'
